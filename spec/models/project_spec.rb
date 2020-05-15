3d82933b3a4b913c6f9e1682a9eca606446a6302
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-05-15 10:39:56 -0600

    Add number_of_contestants model method and test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -12,4 +12,17 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  it "can get number of contestants" do
+    challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    project = challenge.projects.create(name: "Litfit", material: "Lamp")
+    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    chris = Contestant.create(name: "chris Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    ContestantProject.create(contestant_id: jay.id, project_id: project.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: project.id)
+    ContestantProject.create(contestant_id: chris.id, project_id: project.id)
+
+    expect(project.number_of_contestants).to eq(3)
+  end
 end
